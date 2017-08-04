4f54c195980645484315bf6ae61fcc0dedebb4d7
Bill Putrino <bputrino@gmail.com>
2017-08-04 16:00:11 -0400

    Added Model and button code

---
+++ AppSwoosh/Model/Player.swift
@@ -0,0 +1,14 @@
+//
+//  Player.swift
+//  AppSwoosh
+//
+//  Created by Bill Putrino on 8/4/17.
+//  Copyright © 2017 imperialNET. All rights reserved.
+//
+
+import Foundation
+
+struct Player {
+    var desiredLeague: String?
+    var selectedSkillLeve: String?
+}
